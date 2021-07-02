0040ef03831954c1c8219eb0a7d5f00470f0bbf5
서지상 <inaki96@naver.com>
2021-07-02 20:25:22 +0900

    cam setting

--- test2/index.js
+++ test2/index.js
@@ -1,5 +1,15 @@
 const gui = new dat.GUI();
 
+const controls = {
+	cameraX: 1,
+	cameraY: 1,
+	cameraZ: 1
+};
+
+gui.add(controls, 'cameraX', -50, 50);
+gui.add(controls, 'cameraY', -50, 50);
+gui.add(controls, 'cameraZ', -50, 50);
+
 window.onload = () => {
 	const sl_pos = {
 		x: -40,
@@ -18,22 +28,40 @@ window.onload = () => {
 	const scene = new THREE.Scene();
 	const camera = new THREE.PerspectiveCamera(45, stageWidth / stageHeight, 0.1, 1000);
 	const renderer = new THREE.WebGLRenderer();
-	const spotLight = new THREE.SpotLight(0xffff00);
+	const spotLight = new THREE.SpotLight(0x2f0f3f);
 
 	spotLight.position.set(sl_pos.x, sl_pos.y, sl_pos.z);
 	renderer.setClearColor(0xffffff, 1.0);
 	renderer.setSize(stageWidth, stageHeight);
 	renderer.shadowMap.enabled = true;
 
-	const planeGeometry = new THREE.PlaneGeometry(plane_info.width, plane_info.height);
+	const planeGeometry = new THREE.PlaneGeometry(plane_info.width, plane_info.height, 1, 1);
 	const planeMaterial = new THREE.MeshLambertMaterial({ color: plane_info.color });
 	const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
+	plane.rotation.x = -0.5 * Math.PI;
+	setObjPosition(plane, 0, 0, 0);
+
+	setObjPosition(camera, controls.cameraX, controls.cameraY, controls.cameraZ);
+	camera.lookAt(scene.position);
 	scene.add(plane);
 	scene.add(camera);
-	scene.add(spotLight);
+
+	//scene.add(spotLight);
 
 	document.getElementById('stage').appendChild(renderer.domElement);
 
-	renderer.render(scene, camera);
+	const animate =() => {
+
+		setObjPosition(camera, controls.cameraX, controls.cameraY, controls.cameraZ);
+		renderer.render(scene, camera);
+		requestAnimationFrame(animate);
+	}
+	animate();
+};
+
+const setObjPosition = (o, x, y, z) => {
+	o.position.x = x;
+	o.position.y = y;
+	o.position.z = z;
 };
